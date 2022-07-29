bb067a393aa9211dd4c4aa6472753be906077f63
Natalia Hernandez <natalia.hernandez@clarity.ai>
2022-07-29 15:43:51 +0200

    Add spring boot project

---
+++ src/test/java/test/app/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package test.app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
